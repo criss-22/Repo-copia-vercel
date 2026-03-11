cb5718187a8da29a5849be438b514077ff6020ac
Cristopher Osorio Morales <20241042@uthh.edu.mx>
2026-03-11 14:08:27 -0600

    se cambio el nombre del procedimiento

--- src/models/terminos.model.js
+++ src/models/terminos.model.js
@@ -2,8 +2,8 @@ import db from '../config/db.js';
 
 export const obtenerTerminosActivos = async () => {
   // Llamamos al procedimiento que mencionas para obtener título y contenido
-    const [rows] = await db.query('CALL sp_obtener_terminos_activos()');
-    
+    const [rows] = await db.query('CALL CrearNuevoTermino()');
+
   // MySQL devuelve los resultados dentro de un array anidado cuando se usa CALL
-  return rows[0][0]; 
+    return rows[0][0]; 
 };
